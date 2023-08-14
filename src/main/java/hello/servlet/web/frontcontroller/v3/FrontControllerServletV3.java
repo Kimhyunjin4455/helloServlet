b3612475f2bca6550de495bd09a639019a8c54cf
Kimhyunjin4455 <dp4455@naver.com>
2023-08-14 19:18:34 +0900

    0814Pdf3-029

---
+++ src/main/java/hello/servlet/web/frontcontroller/v3/FrontControllerServletV3.java
@@ -0,0 +1,64 @@
+package hello.servlet.web.frontcontroller.v3;
+
+import hello.servlet.web.frontcontroller.ModelView;
+import hello.servlet.web.frontcontroller.MyView;
+import hello.servlet.web.frontcontroller.v3.controller.MemberFormControllerV3;
+import hello.servlet.web.frontcontroller.v3.controller.MemberListControllerV3;
+import hello.servlet.web.frontcontroller.v3.controller.MemberSaveControllerV3;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+@WebServlet(name = "FrontControllerServletV3", urlPatterns = "/front-controller/v3/*")
+public class FrontControllerServletV3 extends HttpServlet {
+
+    private Map<String, ControllerV3> controllerMap = new HashMap<>();
+    public FrontControllerServletV3() {
+        controllerMap.put("/front-controller/v3/members/new-form", new MemberFormControllerV3());
+        controllerMap.put("/front-controller/v3/members/save", new MemberSaveControllerV3());
+        controllerMap.put("/front-controller/v3/members", new MemberListControllerV3());
+    }
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("FrontControllerServletV3.service");
+
+        // /front-controller/v3/members
+        String requestURI = request.getRequestURI();
+
+        ControllerV3 controller = controllerMap.get(requestURI); // ControllerV1 controller = new MemberListControllerV1 가능(다형성)
+        if (controller == null){
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            return;
+        }
+
+        //paramMap
+        Map<String, String> paramMap = createParamMap(request); // 옵션 커맨드 m (+드래그) > 함수 생성
+        ModelView mv = controller.process(paramMap);// 커맨드 옵션 v
+
+        // new-form
+        String viewName = mv.getViewName();// 논리 이름 new-form
+
+        // "/WEB-INF/views/new-form.jsp"
+        MyView view = viewResolver(viewName); // 옵션 커맨드 m >> 메소드 생성,분리
+
+        view.render(mv.getModel(), request, response);
+
+    }
+
+    private static MyView viewResolver(String viewName) {
+        return new MyView("/WEB-INF/views/" + viewName + ".jsp");
+    }
+
+    private static Map<String, String> createParamMap(HttpServletRequest request) {
+        Map<String, String> paramMap = new HashMap<>();
+        request.getParameterNames().asIterator()
+                .forEachRemaining(paramName -> paramMap.put(paramName, request.getParameter(paramName)));
+        return paramMap;
+    }
+}
